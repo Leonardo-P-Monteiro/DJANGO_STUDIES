5ca45d3b3a6beb83c47c6541ecfd9746dce40858
Leonardo P. Monteiro <dev.pmonteiro@gmail.com>
2024-11-07 15:34:29 -0300

    final commit
    Project completed.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
 from . import views
 
+app_name = 'blog'
 
+# blog/
 urlpatterns = [
-    path('', views.blog),
-    path('blog_second', views.blog_second),
+    path('', views.blog, name = 'home'),
+    path('blog_second', views.blog_second, name = 'blog_second'),
+    path('<int:post_id>/', views.post, name = 'post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from blog.data import posts
+from typing import Any
+from django.http import Http404
 
 # Create your views here.
 
@@ -7,9 +10,42 @@ def blog(request):
     This function based view is only to test what we learn on the class.
     You can delete it if you want. 
     """
+    context = {
+        'key': 'database',
+        'posts': posts
+    }
+    
     return render(
         request,
-        template_name='blog/index.html'
+        template_name='blog/index.html',
+        context= context
+    )
+
+def post(request, post_id):
+    """
+    This function based view is only to test what we learn on the class.
+    You can delete it if you want. 
+    """
+    found_post: dict[str, Any] | None  = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+    
+    if found_post is None:
+        raise Http404('Post not found.')
+
+    context = {
+        'title': found_post['title'],
+        'post': found_post
+    }
+
+
+    return render(
+        request,
+        template_name='blog/post.html',
+        context= context
     )
 
 def blog_second(request):
@@ -21,3 +57,4 @@ def blog_second(request):
         request,
         template_name='blog/blog_second.html'
     )
+
